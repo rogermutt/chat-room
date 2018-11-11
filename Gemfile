6a09ae91553ef815cdaa69598545cadddb4d3a1b
Roger Garcia <r1ger_pg@hotmail.com>
2018-11-11 10:11:43 +0800

    Cloned boilerplate code

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-ruby '2.3.0'
+ruby '2.4.4'
 gem 'rails', '>= 5.0.0.beta3', '< 5.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
@@ -24,7 +24,7 @@ gem 'redis', '~> 3.0'
 
 
 
-group :production do 
+group :production do
   gem 'rails_12factor'
 end
 
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,13 +5,13 @@ class RegistrationsController < ApplicationController
     @user = User.new
   end
 
-  def create 
+  def create
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
       redirect_to chatrooms_path
     else
-      redirect_to signup_path, flash[:notice] =  user.errors.messages
+      redirect_to signup_path, flash[:notice] = user.errors.messages
     end
   end
 
